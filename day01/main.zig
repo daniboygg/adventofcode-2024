44f611448e1f4bf4db3ac59a440afffe39276be5
daniboygg <danigayosog@gmail.com>
2024-12-02 11:28:05 +0100

    Simplify file read into buffer
    * Ignore empty lines

--- day01/main.zig
+++ day01/main.zig
@@ -13,17 +13,16 @@ pub fn main() !void {
         }
     }
 
-    const file = try std.fs.cwd().openFile("input.txt", .{});
-    defer file.close();
-
-    const stat = try file.stat();
-    const buffer = try file.readToEndAlloc(allocator, stat.size);
+    const buffer = try std.fs.cwd().readFileAlloc(allocator, "input.txt", 1024 * 1024);
     defer allocator.free(buffer);
 
     var lines = std.ArrayList([]const u8).init(allocator);
     defer lines.deinit();
     var iter = std.mem.split(u8, buffer, "\n");
     while (iter.next()) |line| {
+        if (line.len == 0) {
+            continue;
+        }
         try lines.append(line);
     }
 
--- zig_template.zig
+++ zig_template.zig
@@ -13,17 +13,16 @@ pub fn main() !void {
         }
     }
 
-    const file = try std.fs.cwd().openFile("input.txt", .{});
-    defer file.close();
-
-    const stat = try file.stat();
-    const buffer = try file.readToEndAlloc(allocator, stat.size);
+    const buffer = try std.fs.cwd().readFileAlloc(allocator, "input.txt", 1024 * 1024);
     defer allocator.free(buffer);
 
     var lines = std.ArrayList([]const u8).init(allocator);
     defer lines.deinit();
     var iter = std.mem.split(u8, buffer, "\n");
     while (iter.next()) |line| {
+        if (line.len == 0) {
+            continue;
+        }
         try lines.append(line);
     }
 
@@ -34,10 +33,10 @@ pub fn main() !void {
     std.debug.print("Result 2: {d}\n", .{result});
 }
 
-fn first(allocator: std.mem.Allocator, lines: [][]const u8) !i32 {
+fn first(_: std.mem.Allocator, _: [][]const u8) !i32 {
     return 0;
 }
 
-fn second(allocator: std.mem.Allocator, lines: [][]const u8) !i32 {
+fn second(_: std.mem.Allocator, _: [][]const u8) !i32 {
     return 0;
 }
