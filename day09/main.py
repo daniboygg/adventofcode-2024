42a6559db9e14dd038a576df34177b8b6a9f497b
daniboygg <danigayosog@gmail.com>
2024-12-10 01:08:01 +0100

    Improve performance of day 9 part 1

--- day09/main.py
+++ day09/main.py
@@ -1,13 +1,22 @@
 import collections
+import sys
 
 
-def main():
+def main(make_tests):
+    if make_tests:
+        with open("input_test.txt", "r") as f:
+            data = f.readlines()
+
+        result = first(data)
+        assert result == 1928, f"Expected 1, actual {result}"
+        result = first(data)
+        assert result == 2858, f"Expected 1, actual {result}"
+
     with open("input.txt", "r") as f:
         data = f.readlines()
 
-    # result = first(data)
-    # print(f"Result 1: {result}")
-
+    result = first(data)
+    print(f"Result 1: {result}")
     result = second(data)
     print(f"Result 2: {result}")
 
@@ -27,30 +36,26 @@ def first(lines: list[str]):
             for _ in range(int(value)):
                 disk_map.append(".")
 
-    wholes = {}
-    whole_detector_index = None
+    wholes = []
     for index, value in enumerate(disk_map):
-        if value != ".":
-            whole_detector_index = None
-            continue
-
-        if whole_detector_index:
-            wholes[whole_detector_index] += 1
-        else:
-            whole_detector_index = index
-            wholes[whole_detector_index] = 1
+        if value == ".":
+            wholes.append(index)
 
     for index, value in enumerate(reversed(disk_map)):
-        if is_valid(disk_map):
+        if len(wholes) == 0:
             break
 
         real_index = len(disk_map) - 1 - index
-        try:
-            leftmost_space = disk_map.index(".")
-            disk_map[leftmost_space] = value
-            disk_map[real_index] = "."
-        except ValueError:
-            pass
+        if disk_map[real_index] == ".":
+            continue
+
+        i = wholes.pop(0)
+        if i > real_index:
+            wholes.clear()
+            break
+
+        disk_map[i] = disk_map[real_index]
+        disk_map[real_index] = "."
 
     total = 0
     for index, value in enumerate(disk_map):
@@ -59,6 +64,14 @@ def first(lines: list[str]):
         total += index * int(value)
 
     return total
+
+
+def is_valid(disk_map):
+    leftmost_space = disk_map.index(".")
+    for i in disk_map[leftmost_space:]:
+        if i != ".":
+            return False
+    return True
 
 
 def second(lines: list[str]):
@@ -100,6 +113,7 @@ def second(lines: list[str]):
                 if len(wholes) == 0:
                     break
                 file_size = index - last_number.first_index
+                assert file_size > 0
                 for i in sorted(wholes.keys()):
                     size = wholes[i]
                     if size >= file_size:
@@ -131,17 +145,9 @@ def second(lines: list[str]):
     return total
 
 
-def is_valid(disk_map):
-    leftmost_space = disk_map.index(".")
-    for i in disk_map[leftmost_space:]:
-        if i != ".":
-            return False
-    return True
-
-
 def print_disk_map(value):
     print("".join(value))
 
 
 if __name__ == '__main__':
-    main()
+    main(make_tests=len(sys.argv) > 1 and sys.argv == "--tests")
