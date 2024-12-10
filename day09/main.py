a1410ea4095e60682cb5b608cfe7570d77ed2f03
daniboygg <danigayosog@gmail.com>
2024-12-10 01:45:31 +0100

    Solve day 9 part 2

--- day09/main.py
+++ day09/main.py
@@ -8,9 +8,9 @@ def main(make_tests):
             data = f.readlines()
 
         result = first(data)
-        assert result == 1928, f"Expected 1, actual {result}"
-        result = first(data)
-        assert result == 2858, f"Expected 1, actual {result}"
+        assert result == 1928, f"Expected 1928, actual {result}"
+        result = second(data)
+        assert result == 2858, f"Expected 2858, actual {result}"
 
     with open("input.txt", "r") as f:
         data = f.readlines()
@@ -90,51 +90,53 @@ def second(lines: list[str]):
                 disk_map.append(".")
 
     wholes = {}
-    whole_detector_index = None
+    last_whole_index = None
+    numbers = {}
+    last_number_index = None
+    last_number_value = None
     for index, value in enumerate(disk_map):
-        if value != ".":
-            whole_detector_index = None
-            continue
-
-        if whole_detector_index:
-            wholes[whole_detector_index] += 1
-        else:
-            whole_detector_index = index
-            wholes[whole_detector_index] = 1
-
-    Last = collections.namedtuple("Last", "value first_index")
-    last_number: Last | None = None
-    disk_map_reversed = list(reversed(disk_map))
-    for index, value in enumerate(disk_map_reversed):
-        if last_number:
-            if value == last_number.value:
-                continue
+        if value == ".":
+            if last_whole_index:
+                wholes[last_whole_index] += 1
             else:
-                if len(wholes) == 0:
-                    break
-                file_size = index - last_number.first_index
-                assert file_size > 0
-                for i in sorted(wholes.keys()):
-                    size = wholes[i]
-                    if size >= file_size:
-                        for j in range(i, i + file_size):
-                            disk_map[j] = last_number.value
+                last_whole_index = index
+                wholes[last_whole_index] = 1
 
-                        new_size = wholes[i] - file_size
-                        if new_size > 0:
-                            wholes[i + file_size] = new_size
-                        del wholes[i]
-
-                        for j in range(len(disk_map) - index, len(disk_map) - last_number.first_index):
-                            disk_map[j] = "."
-                        # print_disk_map(disk_map)
-                        break
-                if value != ".":
-                    last_number = Last(value, index)
+            last_number_index = None
+            last_number_value = None
+        else:
+            if last_number_index:
+                if last_number_value == value:
+                    numbers[last_number_index] += 1
                 else:
-                    last_number = None
-        elif value != ".":
-            last_number = Last(value, index)
+                    last_number_index = index
+                    last_number_value = value
+                    numbers[last_number_index] = 1
+            else:
+                last_number_index = index
+                last_number_value = value
+                numbers[last_number_index] = 1
+
+            last_whole_index = None
+
+    number_indexes = sorted(numbers.keys(), reverse=True)
+    for i_number in number_indexes:
+        for i_whole in sorted(wholes.keys()):
+            if i_number < i_whole:
+                break
+
+            whole_size = wholes[i_whole]
+            number_size = numbers[i_number]
+            if whole_size >= number_size:
+                for j in range(i_number, i_number+number_size):
+                    disk_map[i_whole + j - i_number] = disk_map[j]
+                    disk_map[j] = "."
+
+                new_size = whole_size - number_size
+                if new_size > 0:
+                    wholes[i_whole + number_size] = new_size
+                del wholes[i_whole]
+                break
 
     total = 0
     for index, value in enumerate(disk_map):
@@ -150,4 +152,4 @@ def print_disk_map(value):
 
 
 if __name__ == '__main__':
-    main(make_tests=len(sys.argv) > 1 and sys.argv == "--tests")
+    main(make_tests=len(sys.argv) > 1 and sys.argv[1] == "--tests")
