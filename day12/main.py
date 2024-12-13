640f81b0c9c3a5679d405daadb6c7c9b6d0eed5c
daniboygg <danigayosog@gmail.com>
2024-12-13 12:59:03 +0100

    Day 12 part 2 wip

--- day12/main.py
+++ day12/main.py
@@ -10,9 +10,9 @@ def main(make_tests):
         with open("input_test.txt", "r") as f:
             data = f.readlines()
 
-        result = first(data)
-        expected = 1930
-        assert expected == result, f"Result 1: Expected {expected}, actual {result}"
+        # result = first(data)
+        # expected = 1930
+        # assert expected == result, f"Result 1: Expected {expected}, actual {result}"
         result = second(data)
         expected = 1206
         assert expected == result, f"Result 2: Expected {expected}, actual {result}"
@@ -116,8 +116,94 @@ class Region:
         return total_fences
 
     def sides(self, matrix) -> int:
-        # TODO
-        return 0
+        return self._corners(matrix)
+
+    def _corners(self, matrix) -> int:
+        corners = 0
+        if len(self.positions) == 1:
+            return 4
+
+        p_corners = {}
+        for p in self.positions:
+            adjacent = {UP, RIGHT, DOWN, LEFT}
+            for direction in [UP, RIGHT, DOWN, LEFT]:
+                adjacent_p = p.add(direction, matrix)
+                if adjacent_p is None or adjacent_p not in self.positions:
+                    adjacent.remove(direction)
+
+            tmp_corners = corners
+
+            if len(adjacent) == 1:
+                corners += 2
+            elif len(adjacent) == 2:
+                if adjacent == {UP, RIGHT}:
+                    if p.add(Pos(-1, 1), matrix) in self.positions:
+                        corners += 1
+                    else:
+                        corners += 2
+                if adjacent == {RIGHT, DOWN}:
+                    if p.add(Pos(1, 1), matrix) in self.positions:
+                        corners += 1
+                    else:
+                        corners += 2
+                if adjacent == {DOWN, LEFT}:
+                    if p.add(Pos(1, -1), matrix) in self.positions:
+                        corners += 1
+                    else:
+                        corners += 2
+                if adjacent == {LEFT, UP}:
+                    if p.add(Pos(-1, -1), matrix) in self.positions:
+                        corners += 1
+                    else:
+                        corners += 2
+            elif len(adjacent) == 3:
+                if adjacent == {UP, RIGHT, LEFT}:
+                    opposite1 = p.add(Pos(-1, 1), matrix) in self.positions
+                    opposite2 = p.add(Pos(-1, -1), matrix) in self.positions
+                    if not opposite1 and not opposite2:
+                        corners += 2
+                    elif opposite1 ^ opposite2:
+                        corners += 1
+                if adjacent == {UP, RIGHT, DOWN}:
+                    opposite1 = p.add(Pos(1, 1), matrix) in self.positions
+                    opposite2 = p.add(Pos(-1, 1), matrix) in self.positions
+                    if not opposite1 and not opposite2:
+                        corners += 2
+                    elif opposite1 ^ opposite2:
+                        corners += 1
+                if adjacent == {RIGHT, DOWN, LEFT}:
+                    opposite1 = p.add(Pos(1, 1), matrix) in self.positions
+                    opposite2 = p.add(Pos(1, -1), matrix) in self.positions
+                    if not opposite1 and not opposite2:
+                        corners += 2
+                    elif opposite1 ^ opposite2:
+                        corners += 1
+                if adjacent == {DOWN, LEFT, UP}:
+                    opposite1 = p.add(Pos(1, -1), matrix) in self.positions
+                    opposite2 = p.add(Pos(-1, -1), matrix) in self.positions
+                    if not opposite1 and not opposite2:
+                        corners += 2
+                    elif opposite1 ^ opposite2:
+                        corners += 1
+            elif len(adjacent) == 4:
+                corners += 0
+            else:
+                assert False, f"{adjacent} adjacent found, i dont knwow how much cornes i have to count"
+
+            diff = corners - tmp_corners
+            if diff != 0:
+                # print(f"{self.plant}->{p} has {diff} corners")
+                p_corners[p] = diff
+        print(f"{self.plant} -> corners {corners}")
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                if Pos(i, j) in p_corners:
+                    print(f" {matrix[i][j]}{p_corners[Pos(i, j)]}", end="")
+                else:
+                    print(f" {matrix[i][j]} ", end="")
+            print()
+        return corners
 
 
 class Pos(NamedTuple):
@@ -149,9 +235,13 @@ class Pos(NamedTuple):
 
 
 UP = Pos(-1, 0)
+UP_RIGHT = Pos(-1, 1)
 RIGHT = Pos(0, 1)
+DOWN_RIGHT = Pos(1, 1)
 DOWN = Pos(1, 0)
+DOWN_LEFT = Pos(1, -1)
 LEFT = Pos(0, -1)
+UP_LEFT = Pos(-1, -1)
 
 if __name__ == '__main__':
     main(make_tests=len(sys.argv) > 1 and sys.argv[1] == "--tests")
